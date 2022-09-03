f64783c33cc944efff46f15e44f266214576d3ed
Yotam Granada <yotam.granada@gmail.com>
2022-09-03 23:33:40 +0300

    add scrollBy

--- app.js
+++ app.js
@@ -49,3 +49,17 @@ for (let i = 0; i < sections.length; i++){
     })
     navBar.appendChild(newLi)
 }
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+if(isInViewport(sections[2])){
+    navBar[2].
+} 
