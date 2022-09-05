61a6a4b0c2338eaa338dd3370b514046996fbaa4
Yotam Granada <yotam.granada@gmail.com>
2022-09-05 19:51:52 +0300

    working on responsive disply

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-
-
 // const section1 = document.querySelector("#first")
 // const section2 = document.querySelector("#second")
 // const section3 = document.querySelector("#third")
@@ -8,8 +5,6 @@
 // const nav1 = document.querySelector(".first")
 // const nav2 = document.querySelector(".second")
 // const nav3 = document.querySelector(".third")
-
-
 
 // section1.addEventListener('mouseover', function () {
 //     console.log("1")
@@ -29,37 +24,42 @@
 //     document.querySelector(".third").style.backgroundColor = "red"
 // })
 
-const sections = document.querySelectorAll("section")
+const sections = document.querySelectorAll("section");
 console.log(sections);
-const navBar = document.querySelector('#nav-ul');
+const navBar = document.querySelector("#nav-ul");
 
-for (let i = 0; i < sections.length; i++){
-    const newLi = document.createElement('li')
-    const section = sections[i]
-    newLi.textContent = section.dataset.section;
-    
-    newLi.addEventListener('click', function(){
-        console.log(section,newLi)
-        const locationSection = section.getBoundingClientRect()
-        scrollBy({
-            top: locationSection.top - 128, 
-            behavior: "smooth"
+for (let i = 0; i < sections.length; i++) {
+  const newLi = document.createElement("li");
+  newLi.className = "nav-item";
+  const section = sections[i];
+  newLi.textContent = section.dataset.section;
 
-        })
-    })
-    navBar.appendChild(newLi)
+  newLi.addEventListener("click", function () {
+    console.log(section, newLi);
+    const locationSection = section.getBoundingClientRect();
+    scrollBy({
+      top: locationSection.top - 128,
+      behavior: "smooth",
+    });
+  });
+  navBar.appendChild(newLi);
 }
+const navItems = document.querySelectorAll(".nav-item");
+console.log(navItems);
 
 function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+  const rect = element.getBoundingClientRect();
+  return rect.top <= window.innerHeight && rect.bottom >= 0;
 }
-
-if(isInViewport(sections[2])){
-    navBar[2].
-} 
+window.addEventListener("scroll", function () {
+  let stillSearching = true;
+  sections.forEach((section, i) => {
+    console.log(section);
+    if (isInViewport(section) && stillSearching) {
+      stillSearching = false;
+      navItems[i].className = "active-tub";
+    } else {
+      navItems[i].classList.remove("active-tub");
+    }
+  });
+});
